4325e1e78ab7db37a3831bef3545139d7c7c3917
Ed Chaparro <ed@IntrepidProducts.com>
2020-08-28 09:38:58 -0400

    Add solution and project

---
+++ Common/Common/EnumHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace intrepidproducts.common
+{
+    public static class EnumHelper
+    {
+        public static String GetEnumDescription(Enum e)
+        {
+            var enumInfo = e.GetType().GetField(e.ToString());
+
+            DescriptionAttribute[] enumAttributes;
+            try
+            {
+                enumAttributes = (DescriptionAttribute[])enumInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+
+            if (enumAttributes.Length > 0)
+            {
+                return enumAttributes[0].Description;
+            }
+
+            return e.ToString();
+        }
+
+        public static TEnum? GetEnumFromString<TEnum>(string value) where TEnum : struct, IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("TEnum must be an enumerated type");
+            }
+
+            var enumType = typeof(TEnum);
+
+            try
+            {
+                var enumValue = Enum.Parse(enumType, value, true);
+                if (Enum.IsDefined(enumType, enumValue))
+                {
+                    return (TEnum)enumValue;
+                }
+            }
+            catch (ArgumentException)
+            { }
+
+            return null;
+        }
+
+        public static IEnumerable<TEnum> GetAllValues<TEnum>(this TEnum e) where TEnum : struct
+        {
+            return Enum.GetValues(e.GetType()).Cast<TEnum>();
+        }
+    }
+}
