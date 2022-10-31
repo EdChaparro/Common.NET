2e1c39635f1688b9a96cc5b3a27f3c83a48d5ef4
Ed Chaparro <ed@IntrepidProducts.com>
2022-10-31 16:26:08 -0400

    Added Equality methods to Abstract Entity

---
+++ src/Common/AbstractEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace IntrepidProducts.Common
+{
+    public interface IHasId
+    {
+        public Guid Id { get; }
+    }
+
+    public interface IEntity : IHasId
+    { }
+
+    public abstract class AbstractEntity : IHasId, IEntity
+    {
+        protected AbstractEntity(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; } //private setter used by EF (via Reflection).
+
+        #region Equality
+        public override bool Equals(object? obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            var other = obj as AbstractEntity;
+
+            return other != null && Equals(other);
+        }
+
+        protected bool Equals(AbstractEntity other)
+        {
+            return Id.Equals(other.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+        #endregion
+    }
+}
