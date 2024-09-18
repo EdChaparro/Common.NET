edf103f7cf5d22f8afeb2319bc650e73cf456193
Ed Chaparro <ed@IntrepidProducts.com>
2024-09-18 12:12:11 -0400

    Added String Functions to Project
    Migrated from personal exercise solution

---
+++ src/Common/StringsUtil.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace IntrepidProducts.Common
+{
+    public static class StringsUtil
+    {
+        public static string? ToLowerSnakeCase(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+
+            return text
+                .Trim()
+                .Replace(' ', '_')
+                .ToLower();
+        }
+
+        public static string ReverseAndNot(int nbr)
+        {
+            var nbrArray = nbr.ToString().ToCharArray();
+
+            var nbrArrayReversed = nbrArray.Reverse();
+
+            StringBuilder builder = new StringBuilder(nbrArray.Length * 2);
+
+            foreach (var c in nbrArrayReversed)
+            {
+                builder.Append(c);
+            }
+
+            foreach (var c in nbrArray)
+            {
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+
+        public static string ReverseOrderOfWords(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+
+            var words = text.Split(' ').Reverse();
+
+            var results = String.Empty;
+
+            foreach (var word in words)
+            {
+                results = results + " " + word;
+            }
+
+            return results.TrimStart();
+        }
+
+        public static bool IsPalindrome(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                return false;
+            }
+
+            return (text.ToLower() == ReverseOrderOfWords(text).ToLower());
+        }
+
+        /// <summary>
+        /// Based on https://edabit.com/challenge/RFeBL2TzSf7mRMNJi
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string Maskify(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text))
+            {
+                return text;
+            }
+
+            if (text.Length < 5)
+            {
+                return text;
+            }
+
+            var lastFourCharsIndex = text.Length - 4;
+            var lastFourChars = text.Substring(lastFourCharsIndex, 4);
+            var maskedText = string.Empty;
+
+
+            for (int i = 0; i < lastFourCharsIndex; i++)
+            {
+                maskedText = maskedText + "*";
+            }
+
+            return maskedText + lastFourChars;
+        }
+    }
+}
---
+++ src/Tests/Test.Common/StringUtilTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntrepidProducts.Common.Tests
+{
+    [TestClass]
+    public class StringUtilTest
+    {
+        [TestMethod]
+        public void ShouldFormatText()
+        {
+            const string INPUT = "   A B  c    ";
+            const string EXPECTED = "a_b__c";
+
+            var result = StringsUtil.ToLowerSnakeCase(INPUT);
+            Assert.AreEqual(EXPECTED, result);
+        }
+
+        [TestMethod]
+        public void ShouldReverseOrderOfWords()
+        {
+            const string INPUT = "One Two Three";
+            const string EXPECTED = "Three Two One";
+
+            var result = StringsUtil.ReverseOrderOfWords(INPUT);
+            Assert.AreEqual(EXPECTED, result);
+        }
+
+        [TestMethod]
+        public void ShouldIdentifyPalindromeWord()
+        {
+            Assert.IsTrue(StringsUtil.IsPalindrome("Radar"));
+
+            Assert.IsFalse(StringsUtil.IsPalindrome("Not A Palindrome"));
+        }
+
+        [TestMethod]
+        public void ShouldMaskify()
+        {
+            const string INPUT1 = "4556364607935616";
+            const string INPUT2 = "64607935616";
+            const string INPUT3 = "1";
+            const string INPUT4 = "";
+
+            const string EXPECTED_OUTPUT1 = "************5616";
+            const string EXPECTED_OUTPUT2 = "*******5616";
+            const string EXPECTED_OUTPUT3 = "1";
+            const string EXPECTED_OUTPUT4 = "";
+
+            Assert.AreEqual(EXPECTED_OUTPUT1, StringsUtil.Maskify(INPUT1));
+            Assert.AreEqual(EXPECTED_OUTPUT2, StringsUtil.Maskify(INPUT2));
+            Assert.AreEqual(EXPECTED_OUTPUT3, StringsUtil.Maskify(INPUT3));
+            Assert.AreEqual(EXPECTED_OUTPUT4, StringsUtil.Maskify(INPUT4));
+        }
+
+        [TestMethod]
+        public void ShouldReverseAndNot()
+        {
+            const int INPUT1 = 123;
+            const int INPUT2 = 987654321;
+            const int INPUT3 = 1;
+
+            const string EXPECTED_OUTPUT1 = "321123";
+            const string EXPECTED_OUTPUT2 = "123456789987654321";
+            const string EXPECTED_OUTPUT3 = "11";
+
+            Assert.AreEqual(EXPECTED_OUTPUT1, StringsUtil.ReverseAndNot(INPUT1));
+            Assert.AreEqual(EXPECTED_OUTPUT2, StringsUtil.ReverseAndNot(INPUT2));
+            Assert.AreEqual(EXPECTED_OUTPUT3, StringsUtil.ReverseAndNot(INPUT3));
+        }
+    }
+}
